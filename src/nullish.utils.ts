2d1d2b577b3e1b3d5cb5ec99bad66583a64a993f
Jean Paollo Olimpio Gomes Ferreira <jeanpaollo@gmail.com>
2024-07-03 18:35:25 -0300

    1.2.1

--- src/nullish.utils.ts
+++ src/nullish.utils.ts
@@ -2,7 +2,7 @@ export const NIL_VALUES = Object.freeze([null, undefined]);
 
 export const isNil = <T = any>(object: T) => NIL_VALUES.includes(<any>object);
 
-export const isNotNil = <T = any>(object: T) => !isNil(object);
+export const isNotNil = <T = any>(object: T): object is T => !isNil(object);
 
 export const isAnyNil = <T = any>(...args: T[]) =>
   args?.length ? args?.some?.((e) => isNil(e)) : true;
@@ -17,7 +17,9 @@ export const areAllNonNil = <T = any>(...args: T[]) =>
   args?.length ? args?.every?.((e) => isNotNil(e)) : false;
 
 export const coalescing = <T = any>(...args: T[]) =>
-  (args ?? []).find((e) => isNotNil(e)) ?? args[args.length - 1];
+  (args ?? []).find((e) => isNotNil(e)) ?? args.length
+    ? args[args.length - 1]
+    : undefined;
 
 export const firstNonNil = <T = any>(...args: T[]) =>
   (args ?? []).find((e) => isNotNil(e));
